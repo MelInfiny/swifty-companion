ca71e2cc27e1b06435cd17b3a4c6dbeacb9826b4
MelInfiny <70651843+MelInfiny@users.noreply.github.com>
2025-01-03 03:24:03 +0100

    Initial Commit

---
+++ swifty-companion/swifty_companionApp.swift
@@ -0,0 +1,17 @@
+//
+//  swifty_companionApp.swift
+//  swifty-companion
+//
+//  Created by m1 on 03/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct swifty_companionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
